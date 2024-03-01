c39a7e906ebca73dd85d5a584691289b2e1904e5
d8ahazard <d8ahazard@gmail.com>
2024-03-01 16:00:11 -0600

    Logging

--- scripts/facefusion_ui.py
+++ scripts/facefusion_ui.py
@@ -40,6 +40,7 @@ def run_pre_checks(package):
                 if hasattr(module, "MODELS"):
                     for model in module.MODELS:
                         model_path = os.path.dirname(module.MODELS[model]["path"])
+                        print(f"Checking for model: {model} at {model_path}")
                         model_url = module.MODELS[model]["url"]
                         conditional_download(model_path, [model_url])
                 # Recursively check for submodules
