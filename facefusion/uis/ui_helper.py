26bd6b2e4deec2fc373934ba573e516722a0924f
d8ahazard <d8ahazard@gmail.com>
2024-12-10 12:53:14 -0600

    More output/naming fixes

--- facefusion/uis/ui_helper.py
+++ facefusion/uis/ui_helper.py
@@ -20,7 +20,7 @@ def convert_str_none(value: str) -> Optional[str]:
 
 def suggest_output_path(output_directory_path: str, target_path: str) -> Optional[str]:
     if is_image(target_path) or is_video(target_path):
-        _, target_extension = os.path.splitext(target_path)
+        target_name, target_extension = os.path.splitext(target_path)
         output_name = hashlib.sha1(str(state_manager.get_state()).encode('utf-8')).hexdigest()[:8]
-        return os.path.join(output_directory_path, output_name + target_extension)
+        return os.path.join(output_directory_path, f'{target_name}_{output_name}{target_extension}')
     return None
