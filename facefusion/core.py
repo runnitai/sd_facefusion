1f73e24297331757b350048018392200d38c9cbf
d8ahazard <d8ahazard@gmail.com>
2024-12-12 11:19:00 -0600

    Fix args setter

--- facefusion/core.py
+++ facefusion/core.py
@@ -338,7 +338,7 @@ def process_step(job_id: str, step_index: int, step_args: Args) -> bool:
     clear_reference_faces()
     step_total = job_manager.count_step_total(job_id)
     step_args.update(collect_job_args())
-    apply_args(step_args, state_manager.set_item)
+    apply_args(step_args, True)
 
     logger.info(wording.get('processing_step').format(step_current=step_index + 1, step_total=step_total), __name__)
     if common_pre_check() and processors_pre_check():
